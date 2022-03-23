ad3115285c9964b0a809b674f70a149fc7619506
Rodion Sibov <rodionsibov@gmail.com>
2022-03-23 16:01:20 +0200

    first commit

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(json => {
+        console.log(json)
+      })
+  }
+
+}
