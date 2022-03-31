cbc60e37beca3d767d5311342a37d95f1ea81caf
Rodion Sibov <rodionsibov@gmail.com>
2022-04-01 02:42:04 +0300

    first commit

---
+++ src/app/user-list/types/user.ts
@@ -0,0 +1,17 @@
+export interface User {
+    address: {
+      suite: string;
+      city: string;
+      street: string;
+      zipcode: string;
+    };
+    company: {
+      name: string;
+      catchPhrase: string;
+    };
+    email: string;
+    name: string;
+    phone: string;
+    username: string;
+    website: string;
+  }
